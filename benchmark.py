5c102b7c2a46b38debe75317942af2c90cf90c00
Mikko Heikelä <mikko.heikela@gmail.com>
2018-01-10 21:24:22 +0000

    simple numpy matmul benchmark

---
+++ benchmark.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+np.random.seed(123)
+a = np.random.randn(200, 200)
+b = np.random.randn(200, 200)
+c = np.zeros([200,200])
+
+
+def multiply():
+    np.dot(a, b, c)
+
+
+if __name__ == "__main__":
+    import timeit
+    setup = "from __main__ import multiply"
+    print(timeit.timeit("multiply()", setup=setup, number=100))
+
+
+print(c[100, 100])
